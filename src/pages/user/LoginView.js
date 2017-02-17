0802b5dbcff8a6c499be02631a07febbd9cfce44
Paketik0Chaya <paketik0chaya@gmail.com>
2017-02-17 11:36:44 +0700

    Дима проверь время в мозиле

--- src/pages/user/LoginView.js
+++ src/pages/user/LoginView.js
@@ -7,35 +7,34 @@ export default class Header extends React.Component {
 
     render() {
         return (
-
-        <article className="login">
-            <div className="row">
-                <div id="autoriz" className="col-md-3">
-                    <h2>Авторизация</h2>
+            <form className="login">
+                <div className="row">
+                    <div id="autoriz" className="col-md-3">
+                        <h2>Авторизация</h2>
+                    </div>
                 </div>
-            </div>
-            <div id="login" className="row">
-                <div className="col-md-1">
-                    <p>Логин: </p>
+                <div id="login" className="row">
+                    <div className="col-md-1">
+                        <p>Логин: </p>
+                    </div>
+                    <div className="col-md-4">
+                        <input type="text" placeholder="Логин"/>
+                    </div>
                 </div>
-                <div className="col-md-4">
-                    <input type="text" placeholder="Логин"/>
+                <div id="password" className="row">
+                    <div className="col-md-1">
+                        <p>Пароль: </p>
+                    </div>
+                    <div className="col-md-4">
+                        <input type="password" placeholder="************"/>
+                    </div>
                 </div>
-            </div>
-            <div id="password" className="row">
-                <div className="col-md-1">
-                    <p>Пароль: </p>
+                <div id="access" className="row">
+                    <div className="col-md-2">
+                        <button className="btn btn-success" type="submit">Войти</button>
+                    </div>
                 </div>
-                <div className="col-md-4">
-                    <input type="password" placeholder="************"/>
-                </div>
-            </div>
-            <div id="access" className="row">
-                <div className="col-md-2">
-                    <button className="btn btn-success">Войти</button>
-                </div>
-            </div>
-        </article>
+            </form>
         );
     }
 }
